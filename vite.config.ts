f5a9666153ebd9a7c83ad537da37f075a18158cf
Ujjwal <ujjwalrawat2512@gmail.com>
2026-07-26 12:39:58 +0530

    ci: added GitHub Actions workflow for automatic deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Goal-Grid/', // Repository ka exact naam
   plugins: [
     react(),
     tailwindcss(),
